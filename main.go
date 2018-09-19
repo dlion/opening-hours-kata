69fea8a3129acefdb2cc26a32136cfd932456521
Domenico Luciani <domenico.luciani@xpeppers.com>
2018-09-19 13:58:14 +0200

    isOpenOn tests and functionality

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import "time"
+
+type Shop struct {
+	openingDays  []time.Weekday
+	openingHours []int
+}
+
+func (s *Shop) IsOpenOn(t time.Time) bool {
+	if isAnOpeningDay(s.openingDays, t.Weekday()) && isInOpeningHours(s.openingHours, t.Hour()) {
+		return true
+	}
+	return false
+}
+
+func isAnOpeningDay(o []time.Weekday, t time.Weekday) bool {
+	for _, d := range o {
+		if d == t {
+			return true
+		}
+	}
+	return false
+}
+
+func isInOpeningHours(o []int, t int) bool {
+	openingHour := o[0]
+	closingHour := o[1]
+	if t >= openingHour && t <= closingHour {
+		return true
+	}
+	return false
+}
--- main_test.go
+++ main_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+var (
+	wednesday        = time.Date(2016, time.May, 11, 12, 22, 11, 824, time.Local)
+	wednesdayEvening = time.Date(2016, time.May, 11, 23, 22, 11, 824, time.Local)
+	thursday         = time.Date(2016, time.May, 12, 12, 22, 11, 824, time.Local)
+
+	shop = &Shop{
+		openingDays:  []time.Weekday{time.Monday, time.Wednesday, time.Friday},
+		openingHours: []int{8, 16},
+	}
+)
+
+func TestIsOpenOn(t *testing.T) {
+	t.Run("Should be open on an opening day", func(t *testing.T) {
+		expected := true
+		got := shop.IsOpenOn(wednesday)
+
+		if got != expected {
+			t.Errorf("Got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("Should be closed on an closing day", func(t *testing.T) {
+		expected := false
+		got := shop.IsOpenOn(thursday)
+
+		if got != expected {
+			t.Errorf("Got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("Should be closed in a closing hour", func(t *testing.T) {
+		expected := false
+		got := shop.IsOpenOn(wednesdayEvening)
+
+		if got != expected {
+			t.Errorf("Got %v, expected %v", got, expected)
+		}
+	})
 }
